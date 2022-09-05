4b465caa47c769e772101323919ff36c8e222823
tyriongump <tyriongump@gmail.com>
2022-09-05 11:04:26 +1000

    initial commit

---
+++ other-potential-datasource/src/main/java/org/ratman/hermes/producer/VicRoadBlueToothTravelTime.java
@@ -0,0 +1,11 @@
+package org.ratman.hermes.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VicRoadBlueToothTravelTime {
+  public static void main(String[] args) {
+    SpringApplication.run(VicRoadBlueToothTravelTime.class, args);
+  }
+}
---
+++ vicroad-bluetooth-travel-time/src/main/java/org/ratman/hermes/producer/OtherPotentialDatasource.java
@@ -0,0 +1,11 @@
+package org.ratman.hermes.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtherPotentialDatasource {
+  public static void main(String[] args) {
+    SpringApplication.run(OtherPotentialDatasource.class, args);
+  }
+}
