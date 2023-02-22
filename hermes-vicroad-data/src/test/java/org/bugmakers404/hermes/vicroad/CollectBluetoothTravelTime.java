143052daab3f2bcea35dfd62e04c185a880092be
tyriongump <tyriongump@gmail.com>
2023-02-23 01:04:53 +1100

    test framework test

---
+++ hermes-vicroad-data/src/test/java/org/bugmakers404/hermes/vicroad/CollectBluetoothTravelTime.java
@@ -0,0 +1,12 @@
+package org.bugmakers404.hermes.vicroad;
+
+import org.junit.jupiter.api.Test;
+
+public class CollectBluetoothTravelTime {
+
+  @Test
+  public void testGetDataFromVicPlatform() {
+    System.out.println("here");
+  }
+
+}
