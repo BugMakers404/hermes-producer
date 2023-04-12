f6593845971808432e6516b1e100dcc1f2e586f7
tyriongump <tyriongump@gmail.com>
2023-04-12 23:53:41 +1000

    Add docker file to deploy the app in a container.

---
+++ docker/pipeline/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+[[ -z "${JAVA_OPTS}" ]] && JAVA_OPTS="-Xms256m -Xmx1g"
+
+export JAVA_OPTS="${JAVA_OPTS} -XX:+UnlockExperimentalVMOptions -XX:+UseContainerSupport"
+
+exec java ${JAVA_OPTS} -jar athena-backend.jar
